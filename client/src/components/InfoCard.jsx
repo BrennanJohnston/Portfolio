a92978df1d96535fd971f59a5e1b0636a0def92d
Brennan Johnston <skizerz@DESKTOP-9HB7T1J>
2023-11-07 22:06:33 -0600

    Removed info-card scaling, added slide-in

--- client/src/components/InfoCard.jsx
+++ client/src/components/InfoCard.jsx
@@ -7,29 +7,41 @@ function InfoCard(props) {
 
   const [currentBlur, setCurrentBlur] = useState(0);
   const [currentScale, setCurrentScale] = useState(1);
+  const [currentXOffset, setCurrentXOffset] = useState(1);
   const [windowHeight, setWindowHeight] = useState(1);
 
   const blurMax = 100;
-  const scaleMin = 0.7;
+  const scaleMin = 0.5;
+  const maxXOffset = 50; // percent of screen
 
   const ref = useRef();
-
-  console.log(currentScale);
 
   const calculateBlur = () => {
     return Math.max(0, (ref.current.offsetTop - (window.scrollY+window.innerHeight/3*1.5)) / Math.max(1, ref.current.offsetTop)) * blurMax;
   }
 
   const calculateScale = () => {
-    return Math.max(scaleMin, Math.min(1, 1 - (ref.current.offsetTop - window.scrollY-window.innerHeight/3*1.5) / Math.max(1, ref.current.offsetTop)));
+    return 1;//return Math.max(scaleMin, Math.min(1, 1 - (ref.current.offsetTop - window.scrollY-window.innerHeight/3*1.5) / Math.max(1, ref.current.offsetTop)));
+  }
+
+  const calculateXOffset = () => {
+    const scaleFlip = props.alignImageRight ? -1 : 1;
+    return Math.max(0, (ref.current.offsetTop - (window.scrollY+window.innerHeight/3*1.5)) / Math.max(1, ref.current.offsetTop)) * maxXOffset * scaleFlip;
   }
 
   const handleScroll = () => {
     setCurrentBlur(calculateBlur());
     setCurrentScale(calculateScale());
+    setCurrentXOffset(calculateXOffset());
   }
 
   const handleResize = () => {
+    setCurrentBlur(calculateBlur());
+    setCurrentScale(calculateScale());
+    setCurrentXOffset(calculateXOffset());
+  }
+
+  const handleLoad = () => {
     setCurrentBlur(calculateBlur());
     setCurrentScale(calculateScale());
   }
@@ -39,15 +51,17 @@ function InfoCard(props) {
 
     window.addEventListener("scroll", handleScroll);
     window.addEventListener("resize", handleResize);
+    window.addEventListener("load", handleLoad);
 
     return () => {
       window.removeEventListener("scroll", handleScroll);
       window.removeEventListener("resize", handleResize);
+      window.removeEventListener("load", handleLoad);
     }
   }, []);
   
   return (
-    <Container ref={ref} className="info-card" style={{filter: "blur("+currentBlur+"px)", transform: "scale("+currentScale+")"}}>
+    <Container ref={ref} className="info-card" style={{filter: "blur("+currentBlur+"px)", transform: "scale("+currentScale+")", transform: "translateX("+currentXOffset+"vw)"}}>
       <Row>
         <Col sm={12}>
           <div className="info-card-header">
